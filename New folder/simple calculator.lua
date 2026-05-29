dcb79f915227ba295b5a604a027fcdf303354c5b
sakshiguldagad <sakshiguldagad22@gmail.com>
2026-05-29 06:58:51 +0100

    updated code

---
+++ New folder/simple calculator.lua
@@ -0,0 +1,8 @@
+a = float (input(" enter first number:"))
+b = float (input(" enter second number:"))
+
+print("addition=", a+b)
+print ("subtraction=", a-b)
+print("multiplication=", a*b)
+print ("division=", a/b)
+
